4e8ab4cacbeaaa55bc6deff44fbe9dda8be204b2
Deepthi gontla <deepthimedisetty23@gmail.com>
2024-10-08 21:33:08 +0530

    pushing node modules

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,15 +1,15 @@
-import React from "react";
-const Message=(props)=>
-{
-    const c=props.id
-    console.log(props)
-    const {name,className}=props
-    console.log(name,className)
-    return (
-        <div>
-            <h1>Hi {name}</h1>
+// import React from "react";
+// const Message=(props)=>
+// {
+//     const c=props.id
+//     console.log(props)
+//     const {name,className}=props
+//     console.log(name,className)
+//     return (
+//         <div>
+//             <h1>Hi {name}</h1>
 
-        </div>
-    )
-}
-export default Message
+//         </div>
+//     )
+// }
+// export default Message
